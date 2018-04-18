889e00204b09d253c5f3c32a6508ca1624124dd5
ldurazo <ldurazo@mimoni.com>
2018-04-17 19:21:30 -0500

    toy model

--- toys/models.py
+++ toys/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+ 
+class Toy(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=150, blank=False, default='')
+    description = models.CharField(max_length=200, blank=True, default='')
+    toy_category = models.CharField(max_length=200, blank=False, default='')
+    release_date = models.DateTimeField()
+    was_included_in_home = models.BooleanField(default=False)
 
-# Create your models here.
+    class Meta:
+        #It is a tuple
+        ordering = ('name')
